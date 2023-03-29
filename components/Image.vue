ce53d2de8c667a3b066bf38d897d451187db68ec
Jolonte <vinicius.nevesco@gmail.com>
2023-03-29 10:00:26 -0300

    :recycle: refactor

--- components/Image.vue
+++ components/Image.vue
@@ -6,10 +6,10 @@
             <img src="uber.png" alt="uber" />
         </div>
         <div class="flex justify-center items-center w-40 h-40 rounded-2xl hover:bg-purple-400 hover:cursor-pointer bg-purple-500">
-            <img src="netflix.png" alt="uber" />
+            <img src="netflix.png" alt="netflix" />
         </div>
         <div class="flex justify-center items-center w-40 h-40 rounded-2xl hover:bg-purple-400 hover:cursor-pointer bg-purple-500">
-            <img class="w-30 h-30" src="mercado_livre.png" alt="uber" />
+            <img class="w-30 h-30" src="mercado_livre.png" alt="mercado livre" />
         </div>
     </div>
 </template>
