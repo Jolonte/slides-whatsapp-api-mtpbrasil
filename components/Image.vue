b44d2d909b2351684b887d89d4b2fc183b1d727f
Jolonte <vinicius.nevesco@gmail.com>
2023-03-29 10:07:13 -0300

    :recycle: Refactor

--- components/Image.vue
+++ components/Image.vue
@@ -3,13 +3,13 @@
 <template>
     <div class="flex gap-4 w-full h-full justify-center items-center pt-10">
         <div class="flex justify-center items-center w-40 h-40 rounded-2xl hover:bg-purple-400 hover:cursor-pointer bg-purple-500">
-            <img src="uber.png" alt="uber" />
+            <img src="https://logospng.org/download/uber/logo-uber-preta-4096.png" alt="uber" />
         </div>
         <div class="flex justify-center items-center w-40 h-40 rounded-2xl hover:bg-purple-400 hover:cursor-pointer bg-purple-500">
-            <img src="netflix.png" alt="netflix" />
+            <img src="https://proofmart.com/wp-content/uploads/2021/06/598-2-1-2048x1365.png" alt="netflix" />
         </div>
         <div class="flex justify-center items-center w-40 h-40 rounded-2xl hover:bg-purple-400 hover:cursor-pointer bg-purple-500">
-            <img class="w-30 h-30" src="mercado_livre.png" alt="mercado livre" />
+            <img class="w-30 h-30" src="https://logodownload.org/wp-content/uploads/2016/08/mercado-livre-logo-2-1-768x644.png" alt="mercado livre" />
         </div>
     </div>
 </template>
